eb57e8a99d1273675d7dc61311fc9d387d54d99f
rifk <rifuku.kobayashi@gmail.com>
2024-12-16 09:16:33 +0000

    day16 switch to Dijkstra
    Significant performance improvement.
    Part 2 went from nearly 2mins to under 0.5secs.

--- day16/src/lib.rs
+++ day16/src/lib.rs
@@ -1,8 +1,11 @@
-use std::collections::{BTreeMap, HashMap, HashSet};
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap, HashSet},
+};
 
 use anyhow::{anyhow, bail, Result};
 
-#[derive(Clone, Debug, Hash, Eq, PartialEq)]
+#[derive(Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
 enum Dir {
     N,
     S,
@@ -79,43 +82,49 @@ fn parse_input(input: &str) -> Result<Input> {
 pub fn solve_one(input: &str) -> Result<String> {
     let Input { maze, start } = parse_input(input)?;
 
-    let mut routes = BTreeMap::new();
-    routes.insert(0, {
-        let mut set = HashSet::new();
-        set.insert(start);
-        set
-    });
+    let mut heap = BinaryHeap::new();
+    heap.push(Reverse((0, start.clone())));
+
+    let mut shortest = HashMap::new();
+    shortest.insert(start, 0);
+
+    let mut visited = HashSet::new();
 
     let mut end = None;
     while end.is_none() {
-        let (score, set) = routes.pop_first().ok_or_else(|| anyhow!("empty routes"))?;
-        for (i, j, dir) in set {
-            if maze[i][j] == Maze::End {
-                end = Some(score);
-            }
-
-            vec![
-                (score + 1001, dir.turn_cw()),
-                (score + 1001, dir.turn_ccw()),
-                (score + 1, dir),
-            ]
-            .into_iter()
-            .for_each(|(score, dir)| {
-                let (i, j) = dir.step(i, j);
-                if maze[i][j] != Maze::Wall {
-                    routes
-                        .entry(score)
-                        .and_modify(|set| {
-                            set.insert((i, j, dir.clone()));
-                        })
-                        .or_insert_with(|| {
-                            let mut set = HashSet::new();
-                            set.insert((i, j, dir));
-                            set
-                        });
-                }
-            });
+        let (score, (i, j, dir)) = heap.pop().unwrap().0;
+        if maze[i][j] == Maze::End {
+            end = Some(score);
+            continue;
         }
+        if visited.contains(&(i, j, dir.clone())) {
+            continue;
+        }
+
+        vec![
+            (score + 1001, dir.turn_cw()),
+            (score + 1001, dir.turn_ccw()),
+            (score + 1, dir.clone()),
+        ]
+        .into_iter()
+        .for_each(|(score, dir)| {
+            let (i, j) = dir.step(i, j);
+            if maze[i][j] != Maze::Wall {
+                shortest
+                    .entry((i, j, dir.clone()))
+                    .and_modify(|cur_score| {
+                        if score < *cur_score {
+                            heap.push(Reverse((score, (i, j, dir.clone()))));
+                            *cur_score = score;
+                        }
+                    })
+                    .or_insert_with(|| {
+                        heap.push(Reverse((score, (i, j, dir))));
+                        score
+                    });
+            }
+        });
+        visited.insert((i, j, dir));
     }
 
     Ok(end.unwrap().to_string())
@@ -124,62 +133,85 @@ pub fn solve_one(input: &str) -> Result<String> {
 pub fn solve_two(input: &str) -> Result<String> {
     let Input { maze, start } = parse_input(input)?;
 
-    let mut routes = BTreeMap::new();
-    routes.insert(0, {
-        let mut map = HashMap::new();
-        let mut steps = HashSet::new();
-        steps.insert((start.0, start.1));
-        map.insert(start, steps);
-        map
-    });
+    let mut heap = BinaryHeap::new();
+    heap.push(Reverse((0, start.clone())));
+
+    let mut shortest = HashMap::new();
+    shortest.insert(start, (0, HashSet::new()));
+
+    let mut visited = HashSet::new();
 
     let mut end_steps = HashSet::new();
     while end_steps.is_empty() {
-        let (score, map) = routes.pop_first().ok_or_else(|| anyhow!("empty routes"))?;
-        for ((i, j, dir), steps) in map {
-            if maze[i][j] == Maze::End {
-                steps.iter().for_each(|&s| {
-                    end_steps.insert(s);
-                });
-                continue;
+        let (score, (i, j, dir)) = heap.pop().unwrap().0;
+        if maze[i][j] == Maze::End {
+            end_steps.insert((i, j));
+            add_to_path(&mut end_steps, i, j, dir, &shortest);
+            while heap
+                .peek()
+                .map(|peek| peek.0 .0 == score && peek.0 .1 .0 == i && peek.0 .1 .1 == j)
+                .unwrap_or(false)
+            {
+                let (_, (i, j, dir)) = heap.pop().unwrap().0;
+                add_to_path(&mut end_steps, i, j, dir, &shortest);
             }
-
-            vec![
-                (score + 1001, dir.turn_cw()),
-                (score + 1001, dir.turn_ccw()),
-                (score + 1, dir),
-            ]
-            .into_iter()
-            .for_each(|(score, dir)| {
-                let (i, j) = dir.step(i, j);
-                if maze[i][j] != Maze::Wall {
-                    routes
-                        .entry(score)
-                        .and_modify(|map| {
-                            map.entry((i, j, dir.clone()))
-                                .and_modify(|cur_steps| {
-                                    cur_steps.insert((i, j));
-                                    steps.iter().for_each(|&s| {
-                                        cur_steps.insert(s);
-                                    });
-                                })
-                                .or_insert_with(|| {
-                                    let mut steps = steps.clone();
-                                    steps.insert((i, j));
-                                    steps
-                                });
-                        })
-                        .or_insert_with(|| {
-                            let mut set = HashMap::new();
-                            let mut steps = steps.clone();
-                            steps.insert((i, j));
-                            set.insert((i, j, dir), steps);
-                            set
-                        });
-                }
-            });
+            continue;
         }
+        if visited.contains(&(i, j, dir.clone())) {
+            continue;
+        }
+
+        vec![
+            (score + 1001, dir.turn_cw()),
+            (score + 1001, dir.turn_ccw()),
+            (score + 1, dir.clone()),
+        ]
+        .into_iter()
+        .for_each(|(score, next_dir)| {
+            let (next_i, next_j) = next_dir.step(i, j);
+            if maze[next_i][next_j] != Maze::Wall {
+                shortest
+                    .entry((next_i, next_j, next_dir.clone()))
+                    .and_modify(|(cur_score, last)| match score.cmp(cur_score) {
+                        std::cmp::Ordering::Less => {
+                            heap.push(Reverse((score, (next_i, next_j, next_dir.clone()))));
+                            *cur_score = score;
+                            last.insert((i, j, dir.clone()));
+                        }
+                        std::cmp::Ordering::Equal => {
+                            last.insert((i, j, dir.clone()));
+                        }
+                        std::cmp::Ordering::Greater => {}
+                    })
+                    .or_insert_with(|| {
+                        heap.push(Reverse((score, (next_i, next_j, next_dir.clone()))));
+                        let mut last = HashSet::new();
+                        last.insert((i, j, dir.clone()));
+                        (score, last)
+                    });
+            }
+        });
+        visited.insert((i, j, dir));
     }
 
     Ok(end_steps.len().to_string())
+}
+
+type ShortestMap = HashMap<(usize, usize, Dir), (i32, HashSet<(usize, usize, Dir)>)>;
+fn add_to_path(
+    end_path: &mut HashSet<(usize, usize)>,
+    i: usize,
+    j: usize,
+    dir: Dir,
+    shortest: &ShortestMap,
+) {
+    shortest
+        .get(&(i, j, dir))
+        .unwrap()
+        .1
+        .iter()
+        .for_each(|(i, j, dir)| {
+            end_path.insert((*i, *j));
+            add_to_path(end_path, *i, *j, dir.clone(), shortest);
+        });
 }
