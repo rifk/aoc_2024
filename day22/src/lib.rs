64d849070d3f0a815af13706e5421ba94da08433
rifk <rifuku.kobayashi@gmail.com>
2024-12-22 12:09:33 +0000

    use bits for efficiency

--- day22/src/lib.rs
+++ day22/src/lib.rs
@@ -2,20 +2,21 @@ use std::{collections::HashMap, iter};
 
 use anyhow::Result;
 
-fn next_secret(mut secret: i64) -> i64 {
-    secret ^= secret * 64;
-    secret %= 16777216;
-    secret ^= secret / 32;
-    secret %= 16777216;
-    secret ^= secret * 2048;
-    secret %= 16777216;
+const PRUNE_MASK: i32 = 2i32.pow(24) - 1;
+fn next_secret(mut secret: i32) -> i32 {
+    secret ^= secret << 6;
+    secret &= PRUNE_MASK;
+    secret ^= secret >> 5;
+    secret &= PRUNE_MASK;
+    secret ^= secret << 11;
+    secret &= PRUNE_MASK;
     secret
 }
 
 pub fn solve_one(input: &str) -> Result<String> {
     let monkeys = input
         .lines()
-        .map(|v| Ok(v.parse::<i64>()?))
+        .map(|v| Ok(v.parse::<i32>()?))
         .collect::<Result<Vec<_>>>()?;
 
     Ok(monkeys
@@ -23,16 +24,16 @@ pub fn solve_one(input: &str) -> Result<String> {
         .map(|s| {
             iter::successors(Some(s), |s| Some(next_secret(*s)))
                 .nth(2000)
-                .unwrap()
+                .unwrap() as u64
         })
-        .sum::<i64>()
+        .sum::<u64>()
         .to_string())
 }
 
 pub fn solve_two(input: &str) -> Result<String> {
     let monkeys = input
         .lines()
-        .map(|v| Ok(v.parse::<i64>()?))
+        .map(|v| Ok(v.parse::<i32>()?))
         .collect::<Result<Vec<_>>>()?;
 
     Ok(monkeys
@@ -40,13 +41,22 @@ pub fn solve_two(input: &str) -> Result<String> {
         .into_iter()
         .flat_map(|s| {
             iter::successors(Some(s), |s| Some(next_secret(*s)))
+                .map(|s| s % 10)
                 .take(2000)
                 .collect::<Vec<_>>()
                 .windows(2)
-                .map(|w| (w[1] % 10 - w[0] % 10, w[1] % 10))
+                .map(|w| (w[1] - w[0], w[1]))
                 .collect::<Vec<_>>()
                 .windows(4)
-                .map(|w| ((w[0].0, w[1].0, w[2].0, w[3].0), w[3].1))
+                .map(|w| {
+                    let mut k = (w[0].0 + 10) << 5;
+                    k |= w[1].0 + 10;
+                    k <<= 5;
+                    k |= w[2].0 + 10;
+                    k <<= 5;
+                    k |= w[3].0 + 10;
+                    (k, w[3].1)
+                })
                 .rev()
                 .collect::<HashMap<_, _>>()
         })
